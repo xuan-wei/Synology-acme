345465b8528111aa757c18785ef06ea13f4e7481
Xuan Wei <tvrcopgg@gmail.com>
2021-11-09 19:25:03 +0800

    update crt_cp.py to python3 version
    update crt_cp.py to python3 version

--- crt_cp.py
+++ crt_cp.py
@@ -1,4 +1,4 @@
-#!/bin/python2
+#!/bin/python3
 
 # this script copies cert recorded in INFO file from src to des.
 
@@ -26,20 +26,20 @@ try:
     info = json.load(open(INFO_FILE_PATH))
     services = info[SRC_DIR_NAME]['services']
 except:
-    print '[ERR] load INFO file- %s fail' %(INFO_FILE_PATH,) 
+    print('[ERR] load INFO file- %s fail' %(INFO_FILE_PATH,))
     sys.exit(1)
 
 CP_FROM_DIR = ARCHIEV_PATH + '/' + SRC_DIR_NAME
 for service in services:
-    print 'Copy cert for %s' %(service['display_name'])
+    print('Copy cert for %s' %(service['display_name']))
     if service['isPkg']:
         CP_TO_DIR = '%s/%s/%s' %(PKG_CERT_BASE_PATH, service['subscriber'], service['service'])
     else:
         CP_TO_DIR = '%s/%s/%s' % (CERT_BASE_PATH, service['subscriber'], service['service'])
     for f in CERT_FILES:
-        src = CP_FROM_DIR + '/' + f 
+        src = CP_FROM_DIR + '/' + f
         des = CP_TO_DIR + '/' + f
         try:
             shutil.copy2(src, des)
         except:
-            print '[WRN] copy from %s to %s fail' %(src, des)
+            print('[WRN] copy from %s to %s fail' %(src, des))
